2e484fc6070b947f3d27265230a037fcf8ee780a
lendilai <ngethenan768@gmail.com>
2019-05-11 16:55:57 +0300

    Added gameplay page and styled it

--- scripts.js
+++ scripts.js
@@ -10,3 +10,11 @@ $("#piggie").mouseover(function() {
 }).mouseout(function() {
   $(this).attr('src', $(this).data("src"));
 });
+$("#help").click(function() {
+  swal({
+    title: "Piggie Help",
+    text: "1. The player may continue rolling and accumulating points (but risk rolling a 1) or end his turn. \n              2. If the player rolls a 1 his turn is over,he loses all points he accumulated in that turn, and he passes the die to the next player. \n                                                       3. Play passes from player to player until a winner is determined. The winner is the first to get to 100 points !",
+    icon: "info",
+    button: "Got it !",
+  });
+});
